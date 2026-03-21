54ac237652b2c0eb89b218dc5ddc09d8a0ad7be9
Near111111 <jomarmerej@gmail.com>
2026-03-21 22:31:38 +0800

    Update

--- screens/home_screen.dart
+++ screens/home_screen.dart
@@ -336,7 +336,7 @@ class _HomeTabState extends State<_HomeTab> {
   final List<Map<String, String>> _recentSearches = const [
     {'name': 'SM Mall of Asia', 'address': 'Bay City, Pasay'},
     {'name': 'Makati', 'address': 'Ayala Avenue, Makati'},
-    {'name': 'BGC High Street', 'address': 'Bonifacio Global City'},
+    {'name': 'BGC High Street', 'address': 'Bonifacio Global City (BGC)'},
   ];
 
   @override
